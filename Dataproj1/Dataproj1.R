ffc677ece721fdb6b741d1209ebcaf8f64cce03e
Kevin Yao <k.yao.kayoo@gmail.com>
2022-02-09 22:59:31 -0500

    start data project. didnt do chaper 6 bc stapplet

---
+++ Dataproj1/Dataproj1.R
@@ -0,0 +1,3 @@
+dat1 = read.csv("C:/Users/redte/Documents/Stathw/Dataproj1/dataprojlusby4.csv")
+dat2 = read.csv("C:/Users/redte/Documents/Stathw/Dataproj1/dataprojlusby7.csv")
+dat3 = read.csv("C:/Users/redte/Documents/Stathw/Dataproj1/dataprojstein2.csv")
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Stathw
+Kevin Yao's Statistics Homework
+Please don't steal graphs
+Or do
+I don't really care
+NOTE: MAKE YOUR OWN PATHWAYS
+unless i input data manually
