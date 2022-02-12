305b3f2a04a483782a8c361ca3bcecfba0a1e4a2
Kevin Yao <k.yao.kayoo@gmail.com>
2022-02-11 20:36:06 -0500

    i did the outlier part, next is standard distribution. It might not be an update on this file because I actually don't think I will use the file, but rather just use the histograms.

--- Dataproj1/Dataproj1.R
+++ Dataproj1/Dataproj1.R
@@ -14,8 +14,6 @@ ggplot(dat2, aes(x = Handed)) +
 ggplot(dat3[(dat3$Handed != ""),], aes(x = Handed)) +
   xlab("Stein Period 2 Handedness") + 
   geom_bar()
-ggplot(dat1, aes(x = Handed)) + ggplot(dat2, aes(x = Handed)) + 
-  geom_bar(position = position_dodge())
 
 dat4 = read.csv("C:/Users/redte/Documents/Stathw/Dataproj1/ohio.csv")
 print(mean(dat1$Armspan_cm, na.rm = TRUE))
@@ -33,3 +31,19 @@ boxplot(dat1$Armspan_cm, xlab = "Lusby Period 4", ylab = "Arm Span (cm)")
 boxplot(dat4$Armspan_cm, xlab = "Ohio Random Sample", ylab = "Arm Span (cm)")
 hist(dat1$Armspan_cm, xlab = "Lusby Period 4", ylab = "Arm Span (cm)")
 hist(dat4$Armspan_cm, xlab = "Ohio Random Sample", ylab = "Arm Span (cm)")
+
+print(dat1[((dat1$Armspan_cm > 202) | (dat1$Armspan_cm < 130)),]$Armspan_cm)
+print(dat4[((dat4$Armspan_cm > 223.525) | (dat4$Armspan_cm < 94.125)),]$Armspan_cm)
+dat1 = dat1[((dat1$Armspan_cm < 202) & (dat1$Armspan_cm > 130)),]
+dat4 = dat4[((dat4$Armspan_cm < 223.525) & (dat4$Armspan_cm > 94.125)),]
+print(mean(dat1$Armspan_cm, na.rm = TRUE))
+print(sd(dat1$Armspan_cm, na.rm = TRUE))
+print(var(dat1$Armspan_cm, na.rm = TRUE))
+print(quantile(dat1$Armspan_cm, na.rm = TRUE))
+print(IQR(dat1$Armspan_cm, na.rm = TRUE))
+
+print(mean(dat4$Armspan_cm, na.rm = TRUE))
+print(sd(dat4$Armspan_cm, na.rm = TRUE))
+print(var(dat4$Armspan_cm, na.rm = TRUE))
+print(quantile(dat4$Armspan_cm, na.rm = TRUE))
+print(IQR(dat4$Armspan_cm, na.rm = TRUE))
