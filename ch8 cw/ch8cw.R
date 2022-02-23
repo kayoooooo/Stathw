e4b24c89ea4aaba498c260f0c4f08a4964323215
Kevin Yao <k.yao.kayoo@gmail.com>
2022-02-23 10:05:36 -0500

    ch8 classwork num2 done

--- ch8 cw/ch8cw.R
+++ ch8 cw/ch8cw.R
@@ -35,3 +35,32 @@ ggplot(dat, aes(x = Travel_time_to_School, y = residual)) +
 
 dat = read.csv("C:/Users/redte/Documents/Stathw/ch8 cw/data.csv")
 
+dat = dat[((!is.na(dat$Armspan_cm)) & (dat$Armspan_cm > 100)),]
+
+r = cor(dat$Armspan_cm, dat$Height_cm)
+ybar = mean(dat$Armspan_cm) 
+sy = sd(dat$Armspan_cm)
+xbar = mean(dat$Height_cm)
+sx = sd(dat$Height_cm)
+
+b = r * sy / sx
+print(b)
+
+a = ybar - b * xbar
+print(a)
+
+print(lm(dat$Armspan_cm ~ dat$Height_cm))
+summary(lm(dat$Armspan_cm ~ dat$Height_cm))
+
+LSRL = function(x) {
+  return(a + b * x)
+}
+dat = dat %>% mutate(residual = dat$Armspan_cm - LSRL(dat$Height_cm))
+
+ggplot(dat, aes(x = Height_cm, y = Armspan_cm)) + 
+  geom_abline(intercept = a, slope = b) + 
+  geom_point()
+
+ggplot(dat, aes(x = Armspan_cm, y = residual)) + 
+  geom_hline(yintercept = 0) + 
+  geom_point()
