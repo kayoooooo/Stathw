dcaa42695a5d8a4308d6a059d3e6c08acf9eefd9
Kevin Yao <k.yao.kayoo@gmail.com>
2022-02-24 23:03:37 -0500

    ch10 done.

--- ch10/ch10hw.R
+++ ch10/ch10hw.R
@@ -24,5 +24,29 @@ ggplot(dat, aes(x = lift, y = res)) +
   geom_hline(yintercept = 0) + 
   geom_point()
 
+dat = mutate(dat, newx = dat$weight ** (-1/2))
+b = cor(dat$newx,lift) * sd(lift) / sd(dat$newx)
+print(b)
+
+a = mean(lift) - b * mean(dat$newx)
+print(a)
+LSRL = function(x) {
+  return ( a + b * x)
+}
+
+dat = mutate(dat, res2 = dat$lift - LSRL(dat$newx))
+
+ggplot(dat, aes(x = newx, y = lift)) + 
+  geom_point()
+
+
+
+ggplot(dat, aes(x = lift, y = res2)) + 
+  geom_hline(yintercept = 0) + 
+  geom_point()
 
 #Problem 32
+age = c(1:10) * 2
+diameter = c(2.1, 3.9, 5.2, 6.2, 6.9, 7.6, 8.3, 9.1, 10.0, 11.4)
+dat = data.frame(age, diameter)
+summary(lm(dat$diameter ~ dat$age))
