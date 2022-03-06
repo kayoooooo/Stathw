061bf6b465ba6e8169250bd21497795d3e03b135
Kevin Yao <k.yao.kayoo@gmail.com>
2022-03-06 01:13:16 -0500

    Stat data project 2 graphs are up

--- Dataproj2/Dataproj2.R
+++ Dataproj2/Dataproj2.R
@@ -4,15 +4,23 @@ library(dplyr)
 dat = read.csv("C:/Users/redte/Documents/Stathw/Dataproj2/tomatoes.csv")
 
 dat1 = dat[grepl("Comedy", dat$genres),] #any movies that are comedy and maybe other genres
-dat2 = dat[grepl("Action", dat$genres),] #any movies that are action and another genre
+dat2 = dat[grepl("Action", dat$genres),] #any movies that are action and maybe other genres
 
-hist(dat1$tomatometer_rating)
-hist(dat2$tomatometer_rating)
+hist(dat1$tomatometer_rating, xlab = "Tomatometer rating", main = "Tomatometer Ratings of Comedy Movies", ylim = c(0,800))
+hist(dat2$tomatometer_rating, xlab = "Tomatometer rating", main = "Tomatometer Ratings of Action Movies", ylim = c(0,500))
+print(length(dat1[(dat1$tomatometer_rating>80 & dat1$tomatometer_rating<90),]$tomatometer_rating))
 
 ggplot(dat1, aes(x = tomatometer_rating, y = audience_rating)) + 
+  ggtitle("Comedy Movie Rating Comparisons") + 
+  xlab("Tomatometer Rating") + 
+  ylab("Audience Rating") + 
   geom_point()
 
+
 ggplot(dat2, aes(x = tomatometer_rating, y = audience_rating)) + 
+  ggtitle("Action Movie Rating Comparisons") + 
+  xlab("Tomatometer Rating") + 
+  ylab("Audience Rating") + 
   geom_point()
 
 model1 = lm(dat1$audience_rating ~ dat1$tomatometer_rating)
