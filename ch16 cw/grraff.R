28869cce0aa785bcd6987a3bf64d15a5b9881995
Kevin Yao <k.yao.kayoo@gmail.com>
2022-03-29 10:40:40 -0400

    sim hard

--- ch16 cw/grraff.R
+++ ch16 cw/grraff.R
@@ -1,12 +1,12 @@
 library("ggplot2")
 library("dplyr")
-#randm = rnorm(50,17,3.2)
-#print(mean(randm))
-#print(sd(randm))
-#randf = rnorm(50,15,2.8)
-#print(mean(randf))
-#print(sd(randf))
-#already run so no redo
+# randm = rnorm(50,17,3.2)
+# print(mean(randm))
+# print(sd(randm))
+# randf = rnorm(50,15,2.8)
+# print(mean(randf))
+# print(sd(randf))
+# already run so no redo
 
 couple = randm + randf
 dat = data.frame(randm, randf)
@@ -40,3 +40,37 @@ print(cor(descf,sortm))
 dat2 = mutate(dat2, combine = dat2$sortm+dat2$descf)
 print(mean(dat2$combine))
 print(sd(dat2$combine))
+
+#The very hard to do simulation
+
+lowm = sortm[0:10]
+midm = sortm[11:40]
+highm = sortm[41:50]
+lowf = sortf[0:10]
+midf = sortf[11:40]
+highf = sortf[41:50]
+dat3 = data.frame(matrix(ncol = 2, nrow = 0))
+colnames(dat3) = c("m", "f")
+# for (x in c(1:10)) {
+#   rand = sample(1:length(lowf), 1)
+#   print(rand)
+#   dat3[nrow(dat3) + 1,] = c(lowm[x],lowf[rand])
+#   lowf = lowf[-rand]
+# }
+# for (x in c(1:30)) {
+#   rand = sample(1:length(midf), 1)
+#   print(rand)
+#   dat3[nrow(dat3) + 1,] = c(midm[x],midf[rand])
+#   midf = midf[-rand]
+# }
+# for (x in c(1:10)) {
+#   rand = sample(1:length(highf), 1)
+#   print(rand)
+#   dat3[nrow(dat3) + 1,] = c(highm[x],highf[rand])
+#   highf = highf[-rand]
+# }
+print(dat3)
+ggplot(dat = dat3, aes(x = m, y = f)) + 
+  geom_point()
+print(summary(lm(dat3$f ~ dat3$m)))
+print(cor(dat3$f, dat3$m))
