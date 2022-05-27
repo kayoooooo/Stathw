bce4d17cedddaca60cf669dae3c0f429247adf3d
kayoooooo <kevinyao2016@gmail.com>
2022-05-26 20:49:10 -0400

    standardized residuals (i hate r packages

--- ch26/ch26hw.R
+++ ch26/ch26hw.R
@@ -1,7 +1,10 @@
+library(questionr)
+
 #12 test
 x = c(21900, 4058,3898,1333,359,218,4281,806,415,89,12,10)
 dim(x) = c(6,2)
 chisq.test(x)
+chisq.residuals(x)
 
 #20 test
 x = c(142,274,152,396,51,131,244,173,416,51)
